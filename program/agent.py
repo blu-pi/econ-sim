8e5e7c7463b58f28a930daddb41080fc616e40e7
blu-pi <brianbusch2002@hotmail.com>
2022-11-09 16:54:10 +0000

    Began working on making Line graphs with networkx. Added __str__ implementations for buyers and sellers. Fixed imports and specified more functions' return types.

--- program/agent.py
+++ program/agent.py
@@ -1,11 +1,10 @@
-from enum import Enum
 import random
 
 #interface
 class Agent:
 
-    total_sellers = []
-    total_buyers = []
+    sellers_arr = []
+    buyers_arr = []
 
 class Seller(Agent):
 
@@ -15,7 +14,8 @@ class Seller(Agent):
         self.arg_dict = arg_dict
         self.product_price = 0
         self.buyers = []
-        super().total_sellers.append(self)
+        Agent.sellers_arr.append(self)
+        self.arr_pos = len(Agent.sellers_arr) - 1
 
     def setBuyer(self, buyer) -> bool:
         if isinstance(buyer, Buyer):
@@ -31,6 +31,9 @@ class Seller(Agent):
             return self.buyers == other.buyers and self.product_price == other.product_price
         return False
 
+    def __str__(self) -> str:
+        return "Seller" + str(self.arr_pos)
+
 class Buyer(Agent):
 
     valid_args = []
@@ -39,9 +42,10 @@ class Buyer(Agent):
         self.buys_from = buys_from_arr
         self.arg_dict = arg_dict
         self.setPercievedUtility()
-        super().total_buyers.append(self)
+        Agent.buyers_arr.append(self)
+        self.arr_pos = len(Agent.buyers_arr) - 1
 
-    def setPercievedUtility(self, util = False, min_util = 1, max_util = 10):
+    def setPercievedUtility(self, util = False, min_util = 1, max_util = 10) -> None:
         if "percieved_util" in self.arg_dict:
             self.percieved_utility = self.arg_dict["percieved_util"]
         elif util:
@@ -57,3 +61,6 @@ class Buyer(Agent):
         if isinstance(other, Buyer):
             return self.buys_from == other.buys_from and self.percieved_utility == other.percieved_utility
         return False
+
+    def __str__(self) -> str:
+        return "Buyer" + str(self.arr_pos)
--- program/graph.py
+++ program/graph.py
@@ -1,26 +1,49 @@
 import networkx as nx
-from enum import Enum
+import matplotlib.pyplot as plt
 from random import Random
-from agent import Agent 
+from agent import Agent, Buyer, Seller
 
 
-#interface
+#interface, not to be confuced with nx.Graph!
 class Graph:
-    pass
+    
+    total_graphs = []
 
 class Line(Graph):
 
-    def __init__(self, num_sellers, buyer_args = {}, seller_args = {}):
+    def __init__(self, num_sellers, graph_args = {}, buyer_args = {}, seller_args = {}) -> None:
         self.num_sellers = num_sellers
+        self.buyer_args = buyer_args
+        self.seller_args = seller_args
+        self.graph_args = graph_args
+        self.graph_obj = self.makeGraph(num_sellers, buyer_args, seller_args)
 
-    def makeGraph(num_sellers):
+    def makeGraph(self, num_sellers, buyer_args = {}, seller_args = {}) -> nx.Graph:
         G = nx.Graph()
+        Graph.total_graphs.append(G)
+        previous = None
         for i in range(num_sellers):
-            pass
+            seller = Seller(seller_args)
+            G.add_node(str(seller), obj = seller)
+            if i > 0:
+                buyer = Buyer([previous, seller], buyer_args)
+                G.add_edge(str(previous), str(seller), obj = buyer) #add edge between previous and seller. Give buyer object as reference.
+            previous = seller
+        nx.draw(G)
+        return G
 
 class Tree(Graph):
 
-    def __init__(self, num_sellers, arg_dict, buyer_args = {}, seller_args = {}):
+    def __init__(self, num_sellers, graph_args = {}, buyer_args = {}, seller_args = {}) -> None:
         self.num_sellers = num_sellers
-        self.arg_dict = arg_dict
-        super().__init__(num_sellers)
+        self.buyer_args = buyer_args
+        self.seller_args = seller_args
+        self.graph_args = graph_args
+        #TODO makeGraph implementation
+
+if __name__ == "__main__":
+    G = nx.petersen_graph()
+    subax1 = plt.subplot(121)
+    nx.draw(G, with_labels=True, font_weight='bold')
+    subax2 = plt.subplot(122)
+    nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
